3e5f29a7ea1843f1fcd7ac1b6e8cc74a2b925bdd
Hirai0904 <h_dreamarts.co.jp>
2022-07-26 16:48:39 +0900

    中央へスタイル変更

--- src/Number.tsx
+++ src/Number.tsx
@@ -1,14 +1,29 @@
-import react from 'react'
-
-
+import React from "react";
+// import Grid from "@material-ui/core/Grid";
+// const useStyles = makeStyles({
+//   root: {
+//       },
+// });
 interface Props {
-    count:number
+  count: number;
 }
-
-export const Number = (Props: Props ) => {
-// Propsの役割　App>number の親子関係の中で、子にその値を渡す
-    return(<div>
-        
-        クリックした回数：{Props.count}
-    </div>)
-}
+/* <Grid container alignItems="center" justify="center">
+  <Grid item xs={8}></Grid>
+</Grid>; */
+export const Number = (Props: Props) => {
+  // Propsの役割 App>number の親子関係の中で、子にその値を渡す
+  return (
+    <div>
+      <h1
+        style={{
+          color: "green",
+          fontSize: 60,
+          margin: "auto",
+          textAlign: "center",
+        }}
+      >
+        {Props.count} 回
+      </h1>
+    </div>
+  );
+};
