5b4fc7c7c0d0aeaad4e07a06fe4890098b9d7761
Hirai0904 <h_dreamarts.co.jp>
2022-07-26 17:40:25 +0900

    UI全体の調整。ボタンや表示を中央に。

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import { useState } from "react";
 import "./App.css";
+import { makeStyles } from "@material-ui/core/";
+import Grid from "@material-ui/core/Grid";
 import { Number } from "./Number";
 import { Button1 } from "./Button";
 import { Button2 } from "./Buttondown";
+import { Button3 } from "./Buttonreset";
 // importとexportをしっかり定義
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: "yellow",
+      }}
+    >
       <Number count={count} />
-      <Button1
-        setCountup={() => {
-          setCount(count + 1);
-        }}
-      />
-      <Button2
-        setCountdown={() => {
-          return setCount(count - 1);
-        }}
-      />
+      <Grid container spacing={3}>
+        <Grid item xs={4} style={{ textAlign: "center" }}>
+          <Button1
+            setCountup={() => {
+              setCount(count + 1);
+            }}
+          />
+        </Grid>
+        <Grid item xs={4} style={{ textAlign: "center" }}>
+          <Button2
+            setCountdown={() => {
+              return setCount(count - 1);
+            }}
+          />
+        </Grid>
+        <Grid item xs={4} style={{ textAlign: "center" }}>
+          <Button3
+            setCountreset={() => {
+              return setCount(0);
+            }}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }
