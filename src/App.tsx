25ebc967cd547eec641595e16862f3c154d29101
Hirai0904 <h_dreamarts.co.jp>
2022-07-26 16:49:52 +0900

    ボタンの名前変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import { Number } from "./Number";
+import { Button1 } from "./Button";
 import { Button2 } from "./Buttondown";
 // importとexportをしっかり定義
 
 function App() {
-  const [count, setCount]= useState(0);
-  
-
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-     <Number count={count} />
+      <Number count={count} />
+      <Button1
+        setCountup={() => {
+          setCount(count + 1);
+        }}
+      />
       <Button2
         setCountdown={() => {
           return setCount(count - 1);
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,30 +1,28 @@
-import react from "react";
-import{makeStyles} from '@material-ui/core/styles';
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
-    root: {
-        background: 'red',// ボックス背景色
-        border:10,
-        borderRadius: 3,
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        color: 'blue',//文字色
-        height: 48,
-        padding: '0 30px',
-    },
+  root: {
+    background: "lime", // ボックス背景色
+    border: 10,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "green", //文字色
+    height: 48,
+    padding: "0 30px",
+  },
 });
 
 interface Props {
-    setCount: () => void;
-
+  setCountup: () => void;
 }
-export const Button = (Props:Props) =>{
-    const classes = useStyles();
- return <button className={classes.root}
-
-       onClick= {Props.setCount}
-       >
-        button</button>;
-
+export const Button1 = (Props: Props) => {
+  const classes = useStyles();
+  return (
+    <Button size="large" className={classes.root} onClick={Props.setCountup}>
+      up
+    </Button>
+  );
 };
 //  ボタンを押すという動作を作る
