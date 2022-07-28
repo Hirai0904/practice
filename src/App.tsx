aac9c6a9807c915142ec84dae6757594f49baacc
Hirai0904 <h_dreamarts.co.jp>
2022-07-28 15:23:41 +0900

    callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 import { Number } from "./Number";
 import { Button1 } from "./Button";
 // importとexportをしっかり定義
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "yellow", // ボックス背景色,
+    color: "yellow",
+  },
+});
 
 function App() {
   const [count, setCount] = useState(0);
+  const callback1 = useCallback(() => {
+    return setCount(count + 1);
+  }, [count]);
+  const callback2 = useCallback(() => {
+    return setCount(count - 1);
+  }, [count]);
+  const callback3 = useCallback(() => {
+    return setCount(0);
+  }, [0]);
 
+  const classes = useStyles();
   return (
-    <div
-      style={{
-        backgroundColor: "yellow",
-      }}
-    >
+    <div className={classes.root}>
       <Number count={count} />
       <Grid container spacing={3}>
         <Grid item xs={4} style={{ textAlign: "center" }}>
           <Button1
             onClick={() => {
-              return setCount(count + 1);
+              callback1();
             }}
             label={"up"}
           />
@@ -26,7 +39,7 @@ function App() {
         <Grid item xs={4} style={{ textAlign: "center" }}>
           <Button1
             onClick={() => {
-              return setCount(count - 1);
+              callback2();
             }}
             label={"down"}
           />
@@ -34,7 +47,7 @@ function App() {
         <Grid item xs={4} style={{ textAlign: "center" }}>
           <Button1
             onClick={() => {
-              return setCount(0);
+              callback3();
             }}
             label={"reset"}
           />
@@ -45,3 +58,6 @@ function App() {
 }
 
 export default App;
+function count(count: any) {
+  throw new Error("Function not implemented.");
+}
